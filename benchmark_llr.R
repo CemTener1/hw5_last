64b3a1c81cea290ec5b41a6d520774bfc69f7012
Cem Tener <cemtener00@gmail.com>
2023-10-26 19:21:36 -0400

    filled out the functions

---
+++ benchmark_llr.R
@@ -0,0 +1,14 @@
+install.packages("microbenchmark")
+library(microbenchmark)
+
+n = 150
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+
+result <- microbenchmark(
+  llr_result <-llr(x,y,z, omega = 2),
+  times = 100
+)
+
+print(result)
---
+++ test_llr.R
@@ -0,0 +1,42 @@
+setwd("/Users/newyorker/hw5_new/hw5_final/hw5_last")
+source("llr_functions.R")
+#install.packages("testthat")
+library(testthat)
+library(matrixcalc)
+
+
+n = 150
+## a very simple regression model
+x = rnorm(n)
+y = rnorm(x + rnorm(n))
+z = seq(-1, 1, length.out = 100)
+test_that("llr output has correct length", {
+  expect_equal(length(llr(x, y, z, omega = 1)), length(z))
+})
+
+
+test_that("make_weight_matrix works on simple cases", {
+  ## check that the output is a diagonal matrix, that 
+  expect_true(is.diagonal.matrix(make_weight_matrix(omega = 2,x,z)))
+  ## all the elements are positive
+  expect_equal(diag(make_weight_matrix(omega = 2,x,0.2)) >= 0)
+  ## the weights are correct in simple cases where you know 
+  ## what the output shuold be
+  expect_equal(round(
+    diag(
+      make_weight_matrix(omega = 1,c(-0.2,0,0.2),0.2)
+    )
+    ,3),c(0.820,0.976,1))
+})
+
+test_that("make_predictor_matrix works on simple cases", {
+  ## write tests to check that the dimensions are correct, 
+  expect_equal(ncol(make_predicotr_matrix(rnorm(5))),2)
+  expect_equal(nrow(make_predicotr_matrix(rnorm(5))),5)
+  ## the first column is all 1's, etc.
+  expect_equal(make_predicotr_matrix(rnorm(4))[,1], rep(1,4))
+  
+  
+})
+
+#testthat::test_dir(".")
